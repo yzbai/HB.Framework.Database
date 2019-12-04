e18a3e5a6d1acba97f0eae9054bf909c20d5044f
yzbai <yzbai@brlite.com>
2019-12-04 18:04:43 +0800

    xx

--- src/HB.Framework.Database/DatabaseConnectionSettings.cs
+++ src/HB.Framework.Database/DatabaseConnectionSettings.cs
@@ -2,8 +2,8 @@
 {
     public class DatabaseConnectionSettings
     {
-        public bool IsMaster { get; set; } = true;
         public string DatabaseName { get; set; }
         public string ConnectionString { get; set; }
+        public bool IsMaster { get; set; } = true;
     }
 }
