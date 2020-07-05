ba84d7c879d788c66842b8e1abebefa12d2dbc97
yzbai <yzbai@brlite.com>
2020-07-05 20:23:57 +0800

    ss

--- src/HB.Framework.Database/ServicesCollectionExtensions.cs
+++ src/HB.Framework.Database/ServicesCollectionExtensions.cs
@@ -8,6 +8,11 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServicesCollectionExtensions
     {
+        /// <summary>
+        /// Called by Database Infrastructure. ex: MySQL, SQLite
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddDatabase(this IServiceCollection services)
         {
             services.AddSingleton<IDatabaseTypeConverterFactory, DatabaseTypeConverterFactory>();
